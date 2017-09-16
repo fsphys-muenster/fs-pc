5232d04b9f1bae6235e1489845876ad46f7cb4ca
Simon May <simon.may@uni-muenster.de>
2017-09-16 02:51:01 +0200

    Dateien in /etc/skel/ anlegen

--- install_puppet_modules.sh
+++ install_puppet_modules.sh
@@ -2,7 +2,7 @@
 # Installiert alle verwendeten Puppet-Module von Puppet Forge auf
 # nwzpuppet.nwz.wwu.de
 
-path_default='/etc/puppetlabs/code/environments/p2fsphys_test/modules/'
+path_default='/etc/puppetlabs/code/environments/p2fsphys/modules/'
 echo 'Bitte den gewünschten Zielpfad angeben'
 echo "(Voreinstellung: $path_default)"
 read path
@@ -10,13 +10,18 @@ if [ -z "$path" ]; then
 	path="$path_default"
 fi
 for module in \
-	cpick/gdebi \
-	stbenjam/hash_resources \
-	puppetlabs/motd \
-	kb/pam_mount \
+	 puppetlabs/apt \
+	      cpick/gdebi \
+	 camptocamp/gnome \
+	   stbenjam/hash_resources \
+	 puppetlabs/motd \
+	         kb/pam_mount \
 	walkamongus/realmd \
-	ghoneycutt/ssh \
+	 ghoneycutt/ssh \
+	     puppet/unattended_upgrades \
 ; do
 	puppet module install --target-dir "$path" "$module"
 done
+# Schreiben in die Verzeichnisse durch Mitglieder der Gruppe p2fsphys zulassen
+chmod -R g+w "$path"
 
