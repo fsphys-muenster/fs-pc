007797837e4ff2c8083a11aa5380f2aa43f89f1a
Simon May <simon.may@uni-muenster.de>
2017-09-21 19:22:09 +0200

    Gruppe des Home-Verzeichnisses einmalig via pam_exec ändern

--- modules/fsphys/files/srv/fsphys/homedir-set-group.sh
+++ modules/fsphys/files/srv/fsphys/homedir-set-group.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 # Ändert die Gruppe des Home-Verzeichnisses einmalig auf p0fsphys.
+# Ist die Datei ~/.config/fsphys/firstlogin vorhanden, wird keine Änderung
+# durchgeführt.
 
-for homedir in $(ls /home/); do
-	fsphys_config_path="/home/$homedir/.config/fsphys"
-	firstlogin_path="$fsphys_config_path/firstlogin"
-	if [ ! -f "$firstlogin_path" ]; then
-		chgrp p0fsphys "/home/$homedir/"
-		mkdir -p "$fsphys_config_path/"
-		touch "$firstlogin_path"
-	fi
-done
+user=$PAM_USER
+
+# do not run for system users (UID < 1000)
+if [ $(id -u) -lt 1000 ]; then
+	exit
+fi
+fsphys_config_path="/home/$user/.config/fsphys"
+firstlogin_path="$fsphys_config_path/firstlogin"
+if [ ! -f "$firstlogin_path" ]; then
+	chgrp p0fsphys "/home/$user/"
+	sudo -u $PAM_USER mkdir -p "$fsphys_config_path/"
+	sudo -u $PAM_USER touch "$firstlogin_path"
+fi
 
