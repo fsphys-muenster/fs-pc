f5e3bfa5c48df06c73a0cb3750f92ff8245e29a0
Simon May <simon.may@uni-muenster.de>
2017-10-03 16:54:44 +0200

    homedir-set-group.sh verbessern, Gruppe rekursiv setzen

--- modules/fsphys/files/srv/fsphys/homedir-set-group.sh
+++ modules/fsphys/files/srv/fsphys/homedir-set-group.sh
@@ -9,11 +9,19 @@ firstlogin_path="$fsphys_config_path/firstlogin"
 
 # Nicht für System-Accounts ausführen (UID < 1000)
 if [ \( $(id -u $user) -ge 1000 \) -a \( ! -f "$firstlogin_path" \) ]; then
-	chgrp p0fsphys "/home/$user/"
+	# Gruppe rekursiv ändern – aber nicht für die Ordner
+	# Fachschaftsplatte und IVV4_I-Laufwerk
+	find "/home/$user/" -maxdepth 1 \
+		! \( -name Fachschaftsplatte -o -name IVV4_I-Laufwerk \) \
+		-exec chgrp -R p0fsphys {} +
+	# s-Bit setzen, damit alle im Home-Verzeichnis erstellten Dateien die
+	# Gruppe „erben“
+	chmod g+s "/home/$user/"
+	# „Cookie-Datei“ erzeugen
 	mkdir -p "$fsphys_config_path/"
 	touch "$firstlogin_path"
 	# Gibt es einen besseren Weg, hier den Besitzer anzupassen?
-	# (Problematisch z.B., falls ~/.config/ nicht existiert)
+	chown $user:p0fsphys "/home/$user/.config/"
 	chown -R $user:p0fsphys "$fsphys_config_path/"
 fi
 
