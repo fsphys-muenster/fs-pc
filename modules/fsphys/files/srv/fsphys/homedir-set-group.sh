67188e7747a2d5dcf5e8ce5580857f9042a8b167
Simon May <simon.may@uni-muenster.de>
2017-10-21 13:59:08 +0200

    Verzögerung beim 1. Login durch homedir-set-group.sh beheben

--- modules/fsphys/files/srv/fsphys/homedir-set-group.sh
+++ modules/fsphys/files/srv/fsphys/homedir-set-group.sh
@@ -7,21 +7,29 @@ user=$PAM_USER
 fsphys_config_path="/home/$user/.config/fsphys"
 firstlogin_path="$fsphys_config_path/firstlogin"
 
-# Nicht für System-Accounts ausführen (UID < 1000)
-if [ \( $(id -u $user) -ge 1000 \) -a \( ! -f "$firstlogin_path" \) ]; then
-	# Gruppe rekursiv ändern – aber nicht für die Ordner
-	# Fachschaftsplatte und IVV4_I-Laufwerk
-	find "/home/$user/" -maxdepth 1 \
-		! \( -name Fachschaftsplatte -o -name IVV4_I-Laufwerk \) \
-		-exec chgrp -R p0fsphys {} +
-	# s-Bit setzen, damit alle im Home-Verzeichnis erstellten Dateien die
-	# Gruppe „erben“
-	chmod g+s "/home/$user/"
-	# „Cookie-Datei“ erzeugen
-	mkdir -p "$fsphys_config_path/"
-	touch "$firstlogin_path"
-	# Gibt es einen besseren Weg, hier den Besitzer anzupassen?
-	chown $user:p0fsphys "/home/$user/.config/"
-	chown -R $user:p0fsphys "$fsphys_config_path/"
+# Nicht ausführen, wenn:
+# - $PAM_USER nicht gesetzt ist
+# - der Nutzer ein System-Account (UID < 1000) ist
+# - die Datei $fsphys_config_path bereits existiert
+if [ \( -z "$PAM_USER" \) -o
+	\( $(id -u $user) -lt 1000 \) -o
+	\( -f "$firstlogin_path" \) ]
+then
+	exit 0
 fi
+
+# Gruppe rekursiv ändern – aber nicht die Ordner
+# Fachschaftsplatte und IVV4_I-Laufwerk (Netzlaufwerke)
+find "/home/$user/" -maxdepth 1 \
+	! \( -o -name Fachschaftsplatte -o -name IVV4_I-Laufwerk \) \
+	-exec chgrp -hR p0fsphys {} +
+# s-Bit setzen, damit alle im Home-Verzeichnis erstellten Dateien die
+# Gruppe „erben“
+chmod g+s "/home/$user/"
+# „Cookie-Datei“ erzeugen
+mkdir -p "$fsphys_config_path/"
+touch "$firstlogin_path"
+# Gibt es einen besseren Weg, hier den Besitzer anzupassen?
+chown $user:p0fsphys "/home/$user/.config/"
+chown -R $user:p0fsphys "$fsphys_config_path/"
 
