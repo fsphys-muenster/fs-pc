54a91545be3c3183885ffcf2bfe28b0b9900947a
Simon May <simon.may@uni-muenster.de>
2017-10-27 16:09:22 +0200

    In /etc/profile.d/ nicht exit aufrufen!
    Der Aufruf von exit beendet offenbar die komplette Sitzung, sodass ein Login
    von root (z.B. über SSH) sofort wieder beendet wurde.

--- modules/fsphys/files/etc/profile.d/fsphys.sh
+++ modules/fsphys/files/etc/profile.d/fsphys.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 # Nicht für System-Nutzer (UID < 1000) ausführen
-if [ $(id -u) -lt 1000 ]; then
-	exit 0
+if [ $(id -u) -ge 1000 ]; then
+	# Sicherstellen, dass die Verzeichnisse für Firefox und Thunderbird auf dem
+	# Netzlaufwerk existieren, damit die symbolischen Links im Home-Verzeichnis
+	# nicht ins Leere zeigen.
+	mkdir -p ~/IVV4_I-Laufwerk/AppData/Mozilla/Firefox/
+	mkdir -p ~/IVV4_I-Laufwerk/AppData/Thunderbird/
+
+	# Leere sqlite-Dateien aus Firefox-Profilen entfernen.
+	# Diese rufen die Fehlermeldung „Das Lesezeichen- und Chronik-System wird
+	# nicht funktionieren…“ hervor.
+	# S. auch
+	# https://support.mozilla.org/de/kb/Das-Lesezeichen-und-Chronik-System-wird-nicht-funktionieren
+	if [ -d ~/.mozilla/firefox/Profiles/ ]; then
+		find ~/.mozilla/firefox/Profiles/ \
+			-maxdepth 2 \
+			-name '*.sqlite*' -empty \
+			-exec rm {} +
+	fi
 fi
-
-# Sicherstellen, dass die Verzeichnisse für Firefox und Thunderbird auf dem
-# Netzlaufwerk existieren, damit die symbolischen Links im Home-Verzeichnis
-# nicht ins Leere zeigen.
-mkdir -p ~/IVV4_I-Laufwerk/AppData/Mozilla/Firefox/
-mkdir -p ~/IVV4_I-Laufwerk/AppData/Thunderbird/
-
-# Leere sqlite-Dateien aus Firefox-Profilen entfernen.
-# Diese rufen die Fehlermeldung „Das Lesezeichen- und Chronik-System wird
-# nicht funktionieren…“ hervor.
-# S. auch
-# https://support.mozilla.org/de/kb/Das-Lesezeichen-und-Chronik-System-wird-nicht-funktionieren
-find ~/.mozilla/firefox/Profiles/ \
-	-maxdepth 2 \
-	-name '*.sqlite*' -empty \
-	-exec rm {} +
 
