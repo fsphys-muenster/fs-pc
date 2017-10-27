41ca792a911b49c0b59761fec3c84b09f0959d1a
Simon May <simon.may@uni-muenster.de>
2017-10-27 02:13:09 +0200

    /etc/profile.d/fsphys.sh nicht für UID < 1000 ausführen

--- modules/fsphys/files/etc/profile.d/fsphys.sh
+++ modules/fsphys/files/etc/profile.d/fsphys.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+# Nicht für System-Nutzer (UID < 1000) ausführen
+if [ $(id -u) -lt 1000 ]; then
+	exit 0
+fi
+
 # Sicherstellen, dass die Verzeichnisse für Firefox und Thunderbird auf dem
 # Netzlaufwerk existieren, damit die symbolischen Links im Home-Verzeichnis
 # nicht ins Leere zeigen.
 mkdir -p ~/IVV4_I-Laufwerk/AppData/Mozilla/Firefox/
 mkdir -p ~/IVV4_I-Laufwerk/AppData/Thunderbird/
 
-# Entfernen korrupter sqlite-Datenbank aus Firefox-Profil
-find ~/IVV4_I-Laufwerk/AppData/Mozilla/Firefox/Profiles/ -maxdepth 2 -name 'favicons.sqlite*' -empty -exec rm {} +
+# Leere sqlite-Dateien aus Firefox-Profilen entfernen.
+# Diese rufen die Fehlermeldung „Das Lesezeichen- und Chronik-System wird
+# nicht funktionieren…“ hervor.
+# S. auch
+# https://support.mozilla.org/de/kb/Das-Lesezeichen-und-Chronik-System-wird-nicht-funktionieren
+find ~/.mozilla/firefox/Profiles/ \
+	-maxdepth 2 \
+	-name '*.sqlite*' -empty \
+	-exec rm {} +
+
