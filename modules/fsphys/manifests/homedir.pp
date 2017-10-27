8b93f5b4117503ad817148da58e4c46b2b6d8e28
Simon May <simon.may@uni-muenster.de>
2017-10-27 03:04:54 +0200

    Ressourcentyp tidy zu homedir.pp hinzufügen

--- modules/fsphys/manifests/homedir.pp
+++ modules/fsphys/manifests/homedir.pp
@@ -1,22 +1,29 @@
 class fsphys::homedir
 (
 	Hash $file_resources = {},
+	Hash $tidy_resources = {},
 )
 {
-	$::userhomedirs.each |String $home| {
-		$files = Hash($file_resources.map |String $title, Hash $parameters| {
-			# overwrite path parameter if it exists
-			if $parameters[path] {
-				# titles must be unique!
-				["fsphys::homedir-$home-$title",
-					$parameters + {path => "$home/${parameters[path]}"}]
-			}
-			# otherwise, the path is in the title and must be modified there
-			else {
-				["$home/$title", $parameters]
-			}
-		})
-		create_resources(file, $files)
+	$resources = {
+		file => $file_resources,
+		tidy => $tidy_resources,
+	}
+	$resources.each |String $type, Hash $values| {
+		$::userhomedirs.each |String $home| {
+			$home_resources = Hash($values.map |String $title, Hash $parameters| {
+				# overwrite path parameter if it exists
+				if $parameters[path] {
+					# titles must be unique!
+					["fsphys::homedir-$home-$title",
+						$parameters + {path => "$home/${parameters[path]}"}]
+				}
+				# otherwise, the path is in the title and must be modified there
+				else {
+					["$home/$title", $parameters]
+				}
+			})
+			create_resources($type, $home_resources)
+		}
 	}
 }
 
