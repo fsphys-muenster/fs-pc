b0dd5ae3a91ecd929405a7ec7b3c142f5c1a52c7
Simon May <simon.may@uni-muenster.de>
2017-10-28 17:33:29 +0200

    Ausführung von yamllint.sh aus jedem Verzeichnis ermöglichen

--- test/yamllint.sh
+++ test/yamllint.sh
@@ -2,5 +2,6 @@
 # Überprüft alle YAML-Dateien mit yamllint
 # https://github.com/adrienverge/yamllint
 
-find .. -iname '*.yaml' -exec yamllint -c yamllint.conf {} +
+script_path=$(dirname "$0")
+find -iname '*.yaml' -exec yamllint -c "$script_path/yamllint.conf" {} +
 
