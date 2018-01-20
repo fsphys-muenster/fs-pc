cd6a12e432b9cdfa4f5ce7741c5a61ce9e72befd
Simon May <simon.may@uni-muenster.de>
2018-01-20 19:28:28 +0100

    Shell-Skripte standardmäßig im Terminal ausführen

---
+++ modules/fsphys/files/srv/fsphys/run-terminal-emulator/run-terminal-emulator.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Starts a new terminal window and runs the program in $1 within this window.
+# The working directory is set to the directory containing $1.
+
+TEXTDOMAIN=run-terminal-emulator
+TEXTDOMAINDIR=/srv/fsphys/run-terminal-emulator/
+msg=$(gettext -s 'Script execution completed. Press ENTER to close.')
+gnome-terminal --working-directory=$(dirname "$1") -- \
+	sh -c "'$1' ; echo $msg ; read _"
+
